3e1e10aa8571900b6769d9c5477937d44db93f1d
ppz <372301467@qq.com>
2023-11-17 18:23:23 +0800

    feat: dark your web page

---
+++ main.js
@@ -0,0 +1,11 @@
+let toggle_map = {}
+
+chrome.action.onClicked.addListener(async tab => {
+  const dark = !toggle_map[tab.id]
+  toggle_map[tab.id] = dark
+  const job = dark ? 'insertCSS' : 'removeCSS'
+  await chrome.scripting[job]({
+    files: ['dark.css'],
+    target: { tabId: tab.id },
+  })
+})
